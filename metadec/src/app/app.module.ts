d074e8caa5be14a7d89033ae17a46450943ef709
Paula Rodriguez <paularodriguezc08@gmail.com>
2023-02-21 22:04:50 -0500

    creacion componente inicio y login

--- metadec/src/app/app.module.ts
+++ metadec/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
